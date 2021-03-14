74a8428f33d79f4f1f3a82331a43c491390f17d1
JakubMakarewicz <78924900+JakubMakarewicz@users.noreply.github.com>
2021-03-14 17:15:06 +0000

    Update Board.py

--- Chess/Board/Board.py
+++ Chess/Board/Board.py
@@ -5,7 +5,7 @@ import PrintMatrixToConsole as PMC
 class Board:
     def __init__(self):
         self.board: list = [None] * 64
-        self.turn: bool = False
+        self.turn: bool = True
         self.legal_moves = []
         self.fifty_move_rule = 0
         self.move_count = 0
@@ -44,27 +44,3 @@ class Board:
                             piece.en_passant = False
                     else:
                         piece.en_passant = False
-
-
-if __name__ == "__main__":
-    board = Board()
-    board.board[9] = Bishop.Bishop(False, 9)
-    board.board[48] = Pawn.Pawn(False, 48)
-    board.board[36] = Queen.Queen(True, 36)
-    # board.board[32] = Queen.Queen(False, 32)
-    temp = King.King(False, 4)
-    board.board[4] = temp
-    board.board[0] = Rook.Rook(False, 0)
-    board.board[7] = Rook.Rook(False, 7)
-
-    board.board[26] = Pawn.Pawn(False, 26)
-    board.board[25] = Pawn.Pawn(True, 25)
-    board.board[18] = Pawn.Pawn(True, 18)
-    board.board[11] = Pawn.Pawn(True, 11)
-    board.board[33] = Pawn.Pawn(True, 33)
-
-    # board.board[4].make_move(board, 4, 2)
-    board.calculate_all_legal_moves()
-    PMC.print_matrix_to_console(board.board)
-    board.make_move(26, 17)
-    PMC.print_matrix_to_console(board.board)
