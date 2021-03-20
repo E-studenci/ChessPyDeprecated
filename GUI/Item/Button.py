3425faf4210c1c5765f5174822a77ba8ce400141
WojciechCebula <wojtekcebulaa@gmail.com>
2021-03-20 18:39:36 +0100

    Button class improvements

--- GUI/Item/Button.py
+++ GUI/Item/Button.py
@@ -5,12 +5,13 @@ pygame.init()
 
 
 class Button:
-    def __init__(self, position, command, normal_image, hover_image):
+    def __init__(self, position, command, args, normal_image, hover_image):
         self.normal_image = normal_image
         self.hover_image = hover_image
         self.rect = pygame.rect.Rect(position, self.normal_image.get_size())
         self.position = position
         self.command = command
+        self.args = args
 
     def render(self, screen):
         if not self.rect.collidepoint(pygame.mouse.get_pos()):
@@ -21,4 +22,4 @@ class Button:
     def click_event(self, event):
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
             if self.rect.collidepoint(pygame.mouse.get_pos()):
-                self.command[0](self.command[1], self.command[2])
+                self.command(self.args)
