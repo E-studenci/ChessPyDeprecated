06402eec07ae3657af27bb09909a5dc065d6e7a4
WojciechCebula <wojtekcebulaa@gmail.com>
2021-03-25 19:01:55 +0100

    Recursive and non recursive methods for Perft.py

--- Chess/Testing/Perft.py
+++ Chess/Testing/Perft.py
@@ -1,33 +1,46 @@
 import copy
-
+import time
 from Chess.Board.Board import Board
 from Chess.Board.Converters.FenDecoder import initialize_list_from_FEN
+from Chess.Board.PrintMatrixToConsole import print_matrix_to_console
 
 
-def count_legal_moves(boaard, depth):
-    moves = 0
-    while depth > 0:
-        temp = boaard.calculate_all_legal_moves(board.turn)
-        moves += count_vals(temp)
-        for piece in temp:
-            for move in temp[piece]:
-                temp_board = copy.deepcopy(boaard)
+def count_legal_moves_recursive(chess_board, depth):
+    all_legal_moves = 0
+    if depth > 0:
+        current_all_legal_moves = chess_board.calculate_all_legal_moves(chess_board.turn)
+        for piece in current_all_legal_moves:
+            for move in current_all_legal_moves[piece]:
+                if depth == 1:
+                    all_legal_moves += 1
+                temp_board = copy.deepcopy(chess_board)
                 temp_board.make_move(piece.position, move)
-                moves += count_legal_moves(temp_board, depth - 1)
-        depth -= 1
-    return moves
+                all_legal_moves += count_legal_moves_recursive(temp_board, depth - 1)
+    return all_legal_moves
 
 
-def count_vals(temp):
-    count = 0
-    for piece in temp:
-        for move in temp[piece]:
-            count += 1
-    return count
+def count_legal_moves(chess_board, depth):
+    all_legal_moves = 0
+    current_depth = 0
+    queue = [(chess_board, current_depth)]
+    while queue:
+        pop_board, current_depth = queue.pop(0)
+        if current_depth < depth:
+            current_all_legal_moves = pop_board.calculate_all_legal_moves(pop_board.turn)
+            for piece in current_all_legal_moves:
+                for move in current_all_legal_moves[piece]:
+                    if depth == current_depth + 1:
+                        all_legal_moves += 1
+                    temp_board = copy.deepcopy(pop_board)
+                    temp_board.make_move(piece.position, move)
+                    queue.append((temp_board, current_depth + 1))
+    return all_legal_moves
 
 
 if __name__ == '__main__':
     board = Board()
     board.board, board.turn, board.fifty_move_rule, board.move_count \
         = initialize_list_from_FEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
-    print(count_legal_moves(board, 3))
+    start_time = time.time()
+    print(count_legal_moves_recursive(board, 4))
+    print("--- %s seconds ---" % (time.time() - start_time))
