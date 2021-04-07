36a7b0a991d9f8c21aba13fc8f6016f2a77479b3
WojciechCebula <wojtekcebulaa@gmail.com>
2021-04-07 21:22:12 +0200

    Fixed Knight

--- Chess/Pieces/Knight.py
+++ Chess/Pieces/Knight.py
@@ -1,5 +1,6 @@
 from Chess.Pieces.Piece import Piece
 from Chess.Pieces import Constants
+from Chess.Pieces.King import King
 
 
 class Knight(Piece):
@@ -15,14 +16,15 @@ class Knight(Piece):
 
     def calculate_attacked_fields(self, board):
         for index in range(len(self.move_set)):
-            interrupted = False
             maximum_move_length = self.move_set[index]
             current_position = self.position
-            while not interrupted and maximum_move_length > 0:
+            while maximum_move_length > 0:
                 currently_calculated_position = current_position + Constants.KNIGHT_DIRECTION_MATH[index]
                 if currently_calculated_position % Constants.BOARD_SIZE - current_position % Constants.BOARD_SIZE == \
                    Constants.KNIGHT_COLUMN_CHANGE[index] \
                    and 0 <= currently_calculated_position <= len(board.board) - 1:
                     board.attacked_fields[currently_calculated_position] = True
+                    if isinstance(board.board[currently_calculated_position], King):
+                        if board.board[currently_calculated_position].color != self.color:
+                            board.attacked_lines.append({self.position})
                 maximum_move_length -= 1
-
