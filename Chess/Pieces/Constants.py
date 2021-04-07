81ccf7be2bc1049c8d950e69d5ac673ee4549cf9
WojciechCebula <wojtekcebulaa@gmail.com>
2021-04-07 16:53:35 +0200

    Changed

--- Chess/Pieces/Constants.py
+++ Chess/Pieces/Constants.py
@@ -1,5 +1,8 @@
-#                 u, d;  uR,R,dR; uL,L,dL;  uuR,ddR; uuL,ddL;uRR,dRR;uLL,dLL
-DIRECTION_MATH = [8, -8] + [9, 1, -7] + [7, -1, -9] + [17, -15] + [15, -17] + [10, -6] + [6, -10]
-COLUMN_CHANGE = [0, 0] + [1, 1, 1] + [-1, -1, -1] + [1, 1] + [-1, -1] + [2, 2] + [-2, -2]
+#                        u, d;     uR,R,dR;      uL,L,dL
+NORMAL_DIRECTION_MATH = [8, -8] + [9, 1, -7] + [7, -1, -9]
+NORMAL_COLUMN_CHANGE  = [0, 0] + [1, 1, 1] + [-1, -1, -1]
+#                       uuR,ddR;    uuL,ddL;    uRR,dRR;   uLL,dLL
+KNIGHT_DIRECTION_MATH = [17, -15] + [15, -17] + [10, -6] + [6, -10]
+KNIGHT_COLUMN_CHANGE  = [1, 1] + [-1, -1] + [2, 2] + [-2, -2]
 
 BOARD_SIZE = 8
