756859ddfc1800136b7d08ba9c2ac3f63e26638a
makaron <makaron@KOMPUTER>
2021-05-29 21:26:46 +0200

    Using threads - 1 processes

--- GameManagerPackage/Players/PlayerConstructors.py
+++ GameManagerPackage/Players/PlayerConstructors.py
@@ -1,3 +1,5 @@
+import os
+import sys
 from functools import partial
 
 import tensorflow
@@ -5,8 +7,17 @@ import tensorflow
 from GameManagerPackage.Players.Bot import Bot
 from GameManagerPackage.Players.Human import Human
 from GameManagerPackage.Players.PlayerSelectMoveMethods import *
-import Engine.Evaluation as eval_methods
+import Engine.Evaluation as EvalMethods
 
+
+def get_number_of_threads():
+    if sys.platform == 'win32':
+        return int(os.environ['NUMBER_OF_PROCESSORS'])
+    else:
+        return int(os.popen('grep -c cores /proc/cpuinfo').read())
+
+
+available_threads = get_number_of_threads() - 1
 # os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 model = tensorflow.keras.models.load_model('../../Engine/model.h5')
 
@@ -16,28 +27,28 @@ alpha_beta_handcrafted_bot = partial(Bot,
                                      select_move_method=
                                      partial(evaluated_move,
                                              evaluation_method=
-                                             partial(eval_methods.alpha_beta,
+                                             partial(EvalMethods.alpha_beta,
                                                      alpha=-10000,
                                                      beta=10000,
                                                      depthleft=3,
                                                      last_moved_piece=-1,
                                                      eval_function=
-                                                     partial(eval_methods.evaluate,
+                                                     partial(EvalMethods.evaluate,
                                                              PESTO=True,
                                                              evaluate_pawns=False),
                                                      PESTO=True),
-                                             number_of_processes=8))
+                                             number_of_processes=available_threads))
 alpha_beta_neural_bot = partial(Bot,
                                 select_move_method=
                                 partial(evaluated_move,
                                         evaluation_method=
-                                        partial(eval_methods.alpha_beta,
+                                        partial(EvalMethods.alpha_beta,
                                                 alpha=-10000,
                                                 beta=10000,
                                                 depthleft=3,
                                                 last_moved_piece=-1,
                                                 eval_function=
-                                                partial(eval_methods.eval_model,
+                                                partial(EvalMethods.eval_model,
                                                         model=1),
                                                 PESTO=True),
-                                        number_of_processes=8))
+                                        number_of_processes=available_threads))
